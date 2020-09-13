db66897df05445c556f3ef330fdfdb69e690ddec
unknown <raushan.ravi27496@gmail.com>
2020-09-13 14:10:24 +0530

    First Commit

---
+++ src/main/java/com/stacksimplify/restservices/SpringbootBuildingblock1Application.java
@@ -0,0 +1,13 @@
+package com.stacksimplify.restservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBuildingblock1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBuildingblock1Application.class, args);
+	}
+
+}
